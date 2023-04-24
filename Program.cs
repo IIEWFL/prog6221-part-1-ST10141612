810ba1af5e4b1a0241ba67b9064497a1f49e4dbc
unknown <st10141612@vcconnect.edu.za>
2023-04-24 08:42:47 +0200

    1st Commit: Base Ingredient Class

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POE_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Ingredient
+    {
+        string name;
+        double quantity;
+        string unit;
+
+        public Ingredient(string name, double quantity, string unit)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.unit = unit;
+        }
+
+        public string display()
+        {
+            string result = this.quantity.ToString() + " " + this.unit + " " + this.name;
+            return result;
+        }
+    }
+}
