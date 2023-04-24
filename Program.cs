f67fc95362f1617ad678717582b59e14fa89ac02
unknown <st10141612@vcconnect.edu.za>
2023-04-24 09:11:23 +0200

    Recipe Class with addIngredient method

--- Program.cs
+++ Program.cs
@@ -78,4 +78,33 @@ namespace POE_Part1
 
     }
 
-}
+    public class Recipe
+    {
+        public static List<Ingredient> ingredients = new List<Ingredient>();
+        public static List<Step> steps = new List<Step>();
+
+        public static void createRecipe(int num_ing, int num_steps)
+        {
+            void addIngredients()
+            {
+               
+                for (int i = 0; i < num_ing; i++)
+                {
+                    Console.WriteLine("Ingredient Name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Quantity: ");
+                    double quantity = Convert.ToDouble(Console.ReadLine());
+                    Ingredient.originalQuantities.Add(quantity);
+                    Console.WriteLine("Unit of Measure: ");
+                    string unit = Console.ReadLine();
+                    Console.WriteLine();
+                    Ingredient ing = new Ingredient(name, quantity, unit);
+                    ingredients.Add(ing);
+
+                }
+                
+            }
+
+
+
+        }
